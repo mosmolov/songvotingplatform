8b1f05e3a8dae4698ffe2a691f5a97697f3f4923
Michael Osmolovskiy <mosmolovskiy@gmail.com>
2022-04-05 09:37:55 -0400

    add firebase authentication

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,24 @@ import restProvider from 'ra-data-simple-rest'
 import { SongList } from "./components/SongList";
 import { SongCreate } from "./components/SongCreate";
 import { SongEdit } from "./components/SongEdit";
+import {
+  FirebaseAuthProvider
+} from "react-admin-firebase"
 
+const firebaseConfig = {
+  apiKey: "AIzaSyB2vyjZwRQJEiFxAC6x32Wqm-4FG5ABkf0",
+  authDomain: "songvotingplatform.firebaseapp.com",
+  projectId: "songvotingplatform",
+  storageBucket: "songvotingplatform.appspot.com",
+  messagingSenderId: "213547222387",
+  appId: "1:213547222387:web:84d9932c365691a07df106",
+  measurementId: "G-SR7KBH4K21"
+};
+
+FirebaseAuthProvider = FirebaseAuthProvider(firebaseConfig);
 function App(){
   return (
-    <Admin dataProvider={restProvider("http://localhost:3000")}>
+    <Admin authProvider={FirebaseAuthProvider} dataProvider={restProvider("http://localhost:3000")}>
       <Resource name="songs" list={SongList} edit={SongEdit} create={SongCreate}/>
     </Admin>
   )

bed26c274a756bdfaabfe9f6777062fa032fd5c6
Michael Osmolovskiy <mosmolovskiy@gmail.com>
2022-05-03 10:23:03 -0400

    fix voting feature

--- client/src/components/votingcard.js
+++ client/src/components/votingcard.js
@@ -5,53 +5,59 @@ function VotingCard(props) {
   let [songs, setSongs] = useState([]);
 
   const selectSong = async () => {
-
     fetch(`${process.env.REACT_APP_API_URL}/selectedsongs`)
-      .then(response => response.json())
-      .then(data => setSongs(data));
-      
+      .then((response) => response.json())
+      .then((data) => setSongs(data));
   };
   function incrementVoteCount(id) {
     let token = "";
     fetch("https://song-voting-api.herokuapp.com/users/login", {
-        "headers": {
-          "accept": "application/json"
-        },
-        "body": {
-            "email": process.env.REACT_APP_USERNAME,
-            "password": process.env.REACT_APP_PASSWORD
-        },
-        "method": "POST"
-      })
-      .catch(err => console.log(err))
-      .then(response => response.json())
-      .then(data => token = data.token);
-    
-    songs = songs.map((song) => {
-      if (song.id === id) {
-        fetch(`https://song-voting-api.herokuapp.com/selectedSongs/${id}`, {
-          method: "PUT",
-          headers: {
-            "Content-type": "application/json; charset=UTF-8",
-            "Authorization": `Bearer ${token}`
-          },
-          body: JSON.stringify({
-            id: id,
-            artist: song.artist,
-            title: song.title,
-            votes: song.votes++,
-          }),
+      headers: {
+        "accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "email": process.env.REACT_APP_USERNAME,
+        "password": process.env.REACT_APP_PASSWORD,
+      }),
+      method: "POST",
+    })
+      .catch((err) => console.log(err))
+      .then((response) => response.json())
+      .then((data) => {
+        token = data.token;
+        console.log(token);
+        songs = songs.map((song) => {
+          if (song.id === id) {
+            
+            fetch(`https://song-voting-api.herokuapp.com/selectedsongs/${song.id}`, {
+              method: "PUT",
+              headers: {
+                "accept": "application/json",
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+              },
+              body: JSON.stringify({
+                id: id,
+                artist: song.artist,
+                title: song.title,
+                votes: ++song.votes
+              }),
+            });
+          }
+          return song;
         });
-      }
-      return song;
-    });
-    setSongs(songs);
+        selectSong()
+      });
+
+    
+    
   }
   useEffect(() => {
     selectSong();
-    
-  }, []);
 
+  }, []);
+  
   return (
     <div>
       {songs &&
@@ -66,7 +72,7 @@ function VotingCard(props) {
             </Button>
           </Card>
         ))}
-
+      
       <Button variant="success">
         <Link to="/admin">Admin? Login</Link>
       </Button>
@@ -75,3 +81,5 @@ function VotingCard(props) {
 }
 
 export default VotingCard;
+
+

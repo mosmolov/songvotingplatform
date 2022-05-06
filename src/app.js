040369ae43d591e14df5dd8347c63836e1e151b8
Michael Osmolovskiy <mosmolovskiy@gmail.com>
2022-05-05 20:22:51 -0400

    fix whitespace, change bg

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AdminPanel from "./components/admin";
 import HomePage from "./components/homepage";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+  }, []);
   return (
     <div className="App">
       <Router>

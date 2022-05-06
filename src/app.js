1188f90a0eee0e96f6e8ee69bee38d1449d74324
Michael Osmolovskiy <mosmolovskiy@gmail.com>
2022-05-05 22:04:14 -0400

    add new background

--- src/app.js
+++ src/app.js
@@ -3,12 +3,18 @@ import AdminPanel from "./components/admin";
 import HomePage from "./components/homepage";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NET from "vanta/dist/vanta.net.min"
 function App() {
+  function netBackground(){
+    
+  }
   useEffect(() => {
     document.body.style.overflow = "hidden";
   }, []);
   return (
+    
     <div className="App">
+      
       <Router>
       <Routes>
           <Route path="/" element={<HomePage />} />
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import NET from "vanta/dist/vanta.net.min";
+import * as THREE from "three";
 import VotingCard from "./votingcard";
 import { Card } from "react-bootstrap";
 import "react-bootstrap";
 import "../votingcard.css";
 function HomePage(props) {
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const myRef = useRef(null);
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        NET({
+          el: myRef.current,
+          THREE: THREE,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.0,
+          minWidth: 200.0,
+          scale: 1.0,
+          scaleMobile: 1.0,
+          color: 0xffffff,
+          backgroundColor: 0x0,
+          points: 14.0,
+          maxDistance: 24.0,
+          spacing: 13.0,
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
+
   return (
-    <div className="home-page">
+    <div ref={myRef} className="home-page" id="home-page">
       <Card className="card" bg="transparent" border="light" text="light">
-      <Card.Title className="card-title">Vote For Your Favorite Song Below</Card.Title>
+        <Card.Title className="card-title">
+          Vote For Your Favorite Song Below
+        </Card.Title>
         <VotingCard />
       </Card>
     </div>
